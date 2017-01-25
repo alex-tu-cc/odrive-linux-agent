b63bef9648e430aa36050b41f0f1d1bd4617a6af
Alex Tu <alex.tu@canonical.com>
2017-01-25 11:43:35 +0800

    try to pack odrive agent

---
+++ debian/odrive-linux-agent.default.ex
@@ -0,0 +1,10 @@
+# Defaults for odrive-linux-agent initscript
+# sourced by /etc/init.d/odrive-linux-agent
+# installed at /etc/default/odrive-linux-agent by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ readme.md
@@ -0,0 +1,4 @@
+## cloud aggregator
+ * [odrive](https://www.odrive.com)
+     * [scripts in linux](https://forum.odrive.com/t/odrive-sync-agent-a-cli-scriptable-interface-for-odrives-progressive-sync-engine-for-linux-os-x-and-windows/499)
+ 
